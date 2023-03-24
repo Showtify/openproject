b6baaf75811fcfbb7eea3609f8cfc052ea684687
ulferts <jens.ulferts@googlemail.com>
2023-03-24 17:08:45 +0100

    wip - write data migrations

--- db/migrate/20230322135932_merge_wiki_content_into_page.rb
+++ db/migrate/20230322135932_merge_wiki_content_into_page.rb
@@ -1,15 +1,86 @@
 class MergeWikiContentIntoPage < ActiveRecord::Migration[7.0]
   def change
     change_table :wiki_pages, bulk: true do |t|
-      t.references :author, index: true, null: true, foreign_key: { to_table: :users }# TODO: switch to not null
+      t.references :author, index: true, null: true, foreign_key: { to_table: :users }
       t.text :text, limit: 16.megabytes
-      t.integer :lock_version, null: false # TODO: switch to not null
+      t.integer :lock_version, null: true
     end
 
     add_index :wiki_pages, :updated_at
 
+    reversible do |dir|
+      dir.up do
+        execute <<~SQL.squish
+          UPDATE
+            wiki_pages
+          SET
+            text = wiki_contents.text,
+            author_id = wiki_contents.author_id,
+            lock_version = wiki_contents.lock_version
+          FROM
+            wiki_contents
+          WHERE
+            wiki_contents.page_id = wiki_pages.id
+        SQL
 
-    # TODO: migrate data
+        execute <<~SQL.squish
+          UPDATE
+            journals
+          SET
+            journable_id = wiki_contents.page_id,
+            journable_type = 'WikiPage',
+            data_type = 'Journal::WikiPageJournal'
+          FROM
+            wiki_contents
+          WHERE
+            journals.journable_id = wiki_contents.id
+          AND
+            journals.journable_type = 'WikiContent'
+          AND
+            journals.data_type = 'Journal::WikiContentJournal'
+        SQL
+      end
+      dir.down do
+        execute <<~SQL.squish
+          INSERT INTO
+            wiki_contents (
+              page_id,
+              author_id,
+              text,
+              updated_at,
+              lock_version
+            )
+          SELECT
+            id,
+            author_id,
+            text,
+            updated_at,
+            lock_version
+          FROM
+            wiki_pages
+        SQL
+
+        execute <<~SQL.squish
+          UPDATE
+            journals
+          SET
+            journable_id = wiki_contents.id,
+            journable_type = 'WikiContent',
+            data_type = 'Journal::WikiContentJournal'
+          FROM
+            wiki_contents
+          WHERE
+            journals.journable_id = wiki_contents.page_id
+          AND
+            journals.journable_type = 'WikiPage'
+          AND
+            journals.data_type = 'Journal::WikiPageJournal'
+        SQL
+      end
+    end
+
+    change_column_null :wiki_pages, :lock_version, false
+    change_column_null :wiki_pages, :author_id, false
 
     drop_table :wiki_contents do |t|
       t.integer :page_id, null: false
@@ -23,8 +94,6 @@ class MergeWikiContentIntoPage < ActiveRecord::Migration[7.0]
       t.index %i[page_id updated_at]
     end
 
-    # TOOD: rename 'WikiContent' to 'WikiPage' in journals 'journable_type'
-    # TOOD: rename 'WikiContentJournal' to 'WikiPageJournal' in journals 'data_type'
 
     rename_table :wiki_content_journals, :wiki_page_journals
 
